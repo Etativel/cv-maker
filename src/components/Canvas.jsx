c7b34c40a43eb16136feee8e57eccd4ccdafe60f
Etativel <farhanmaulana4611@gmail.com>
2024-11-09 20:06:00 +0800

    feat: add sidebar and canvas jsx

---
+++ src/components/Canvas.jsx
@@ -0,0 +1,12 @@
+export default function Canvas() {
+  return (
+    <div className="canvas">
+      <div className="paper">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita, quis
+        vel quidem itaque nam molestiae vero nihil nisi provident, ducimus
+        tempore ad praesentium, dolore harum deleniti laudantium veritatis!
+        Iste, recusandae.
+      </div>
+    </div>
+  );
+}
